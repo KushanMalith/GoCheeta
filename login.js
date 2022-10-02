0e251f6a3b699300b69ea6271dce132b85e5aad2
Kushan M <kushaaanm@gmail.com>
2022-10-02 20:17:46 +0530

    Added New Changes

--- login.js
+++ login.js
@@ -1,9 +1,7 @@
-document.getElementById("login-btn").addEventListener("click", () => {
-    console.log(document.getElementById("register-email").value)
-    console.log(document.getElementById("register-password").value)
-    
+document.getElementById("register-btn").addEventListener("click", () => {
     const data = {
-        id: 128192,
+        id: Math.random().toString(16).slice(2),
+        name: document.getElementById("register-name").value,
         email: document.getElementById("register-email").value,
         mobile: document.getElementById("register-mobile").value,
         password: document.getElementById("register-password").value
@@ -16,6 +14,9 @@ document.getElementById("login-btn").addEventListener("click", () => {
         },
         body : JSON.stringify(data)
     };
-    fetch("http://localhost:8080/go-cheeta-service/customer", options);
+    fetch("http://localhost:8080/go-cheeta-backend/customer", options);
+    window.open(
+        'https://static.wixstatic.com/media/4a1d5b_5361da33eab146479729473ed7a5d8ce~mv2.gif',
+        '_blank' 
+      );
 })
-
